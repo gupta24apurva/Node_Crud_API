0b8554237204ab6d037135b99c2afb04dba7363d
Apurva Kumar Gupta <akgupta24092002@gmail.com>
2023-08-26 00:15:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const http=require("http");
+// require("dotenv").config(); 
+const PORT=process.env.PORT || 5001;
+
+let movies=require("./data/movies.json")
+const getReq=require("./methods/get-request");
+const postReq=require("./methods/post-request");
+const putReq=require("./methods/put-request");
+const deleteReq=require("./methods/delete-request");
+
+const server=http.createServer((req,res)=>{
+    req.movies=movies;
+    switch(req.method){
+        case "GET":
+            getReq(req,res);
+            break;
+        
+        case "POST":
+            postReq(req,res);
+            break;
+
+        case "PUT":
+            putReq(req,res);
+            break;
+        
+        case "DELETE":
+            deleteReq(req,res);
+            break;
+        
+        default:
+            res.statusCode=404;
+            res.setHeader("Content-Type","application/json");
+            res.write(JSON.stringify({title:"Not found",message: "Route not found"}));
+            res.end();
+    }
+})
+
+server.listen(PORT,()=>{
+    console.log(`Server started on PORT ${PORT}`);
+});
